8c5c071487d81c9927a413418dbb4205fcfbaf39
LumaEmu43 <lukacs2002@gmail.com>
2023-11-10 00:51:27 +0100

    all

--- main/main.cpp
+++ main/main.cpp
@@ -9,7 +9,7 @@ LCD PORT            GPIO
 
 RS          ->      2
 E           ->      3
-D4          ->      4
+D4          ->      8
 D5          ->      5
 D6          ->      6
 D7          ->      7
@@ -37,6 +37,9 @@ EV = log10(lux * iso / INCIDENT_CALIBRATION) / log10(2);
 double shutterspeed = (pow(2, EV) / pow(aperature, 2));
 */
 
+#include "esp32-hal-gpio.h"
+#include "esp_err.h"
+#include "hal/gpio_types.h"
 #include <LiquidCrystal.h>
 #include <Wire.h>
 #include <BH1750.h>
@@ -49,6 +52,8 @@ extern "C"
     #include <freertos/FreeRTOS.h>
     #include <freertos/task.h>
     #include "freertos/portmacro.h"
+    #include "esp_wifi.h"
+    #include "esp_sleep.h"
 }
 
 extern "C"
@@ -62,14 +67,15 @@ extern "C"
 #define ISO_N 26        //Number of most common ISO/ASA values
 #define SHUTTER_N 14    //Number of possible shutter speeds
 #define INCIDENT_CALIBRATION 330    // Calibration constant (C) for incident light meters, see https://en.wikipedia.org/wiki/Light_meter#Calibration_constants
+#define BTN_MEASURE 4
 
 
 //Defining objects
 BH1750 lightMeter;      //Light meter sensor using the BH1750 library (might change later)
-LiquidCrystal lcd(2, 3, 4, 5, 6, 7);    //LCD initialization (might change later due to I2C)
+LiquidCrystal lcd(2, 3, 8, 5, 6, 7);    //LCD initialization (might change later due to I2C)
 
 //Defining variables
-long lux;    //Measured LUX value
+long lux = 0;    //Measured LUX value
 int8_t exposure_value;
 float shutter_speed_calculated;
 
@@ -77,9 +83,12 @@ double aperture_array[APERTURE_N] = {1.4, 1.7, 2, 2.4, 2.8, 3.3, 4, 4.8, 5.6, 6.
 float shutter_speed_array[SHUTTER_N] = {1, 1/2.0, 1/4.0, 1/8.0, 1/15.0, 1/30.0, 1/60.0, 1/125.0, 1/250.0, 1/500.0, 1/1000.0, 1/2000.0, 1/4000.0, 1/8000.0};
 int iso_array[ISO_N] = {12, 16, 20, 25, 32, 40, 50, 64, 80, 100, 125, 160, 200, 250, 320, 400, 500, 640, 800, 1000, 1250, 1600, 2000, 2500, 3200, 6400};
 
-
 void app_main(void)
 {
+    //esp_bluedroid_disable();
+    //esp_bt_controller_disable();
+    esp_wifi_stop();
+
     Wire.setPins(0, 1);
     Wire.begin();
     lightMeter.begin(BH1750::CONTINUOUS_HIGH_RES_MODE_2);
@@ -87,23 +96,40 @@ void app_main(void)
     lcd.begin(16, 2);
     lcd.print("LUX value:");
 
+    esp_deep_sleep_enable_gpio_wakeup(1 << BTN_MEASURE,
+    ESP_GPIO_WAKEUP_GPIO_HIGH);
+    //digitalWrite(18,HIGH);
+
+    pinMode(BTN_MEASURE, INPUT);
+    
+    lcd.setCursor(0, 1);
+    lcd.print(lux);
+    
+
+
     while(1)
     {
-        lux = lightMeter.readLightLevel();
+        if(digitalRead(BTN_MEASURE))
+        {
+            lux = lightMeter.readLightLevel();
+            exposure_value = log10(lux * iso_array[9] / INCIDENT_CALIBRATION) / log10(2);
+            shutter_speed_calculated = (pow(2, exposure_value) / pow(aperture_array[4], 2));
 
-        exposure_value = log10(lux * iso_array[9] / INCIDENT_CALIBRATION) / log10(2);
-        shutter_speed_calculated = (pow(2, exposure_value) / pow(aperture_array[4], 2));
+            printf("Current LUX value: %ld; ", lux);
+            printf("Current film speed: %d; ", iso_array[9]);
+            printf("Current f-stop: %f; ", aperture_array[4]);
+            printf("Calculated shutter speed: %f; ", shutter_speed_calculated);
+            printf("Exposure Value is: %d\n", exposure_value);
 
-        printf("Current LUX value: %ld; ", lux);
-        printf("Current film speed: %d; ", iso_array[9]);
-        printf("Current f-stop: %f; ", aperture_array[4]);
-        printf("Calculated shutter speed: %f; ", shutter_speed_calculated);
-        printf("Exposure Value is: %d\n", exposure_value);
+            lcd.setCursor(0, 1);
+            lcd.print(lux);
 
-        lcd.setCursor(0, 1);
-        lcd.print(lux);
-
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+            delay(500);
+        }
+        delay(100);
+        printf("Going to sleep...\n");
+        //delay(1000);
+        esp_deep_sleep_start();
     }
 }
 
